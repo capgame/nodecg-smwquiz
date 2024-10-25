e8135ed28ee3e1462d00982e4e6704fc3e1c38f6
capgame <77314649+capgame@users.noreply.github.com>
2024-10-25 21:20:15 +0900

    Initial commit

---
+++ src/extension/util/replicants.ts
@@ -0,0 +1,26 @@
+/* eslint-disable max-len */
+
+import type { DisplaySound, SetupInformationArray, StartSoon } from '@nodecg-vue-ts-template/types/schemas';
+import type { RunDataActiveRun } from '@nodecg-vue-ts-template/types/schemas/speedcontrol';
+import type NodeCG from '@nodecg/types';
+import { get as nodecg } from './nodecg';
+
+/**
+ * This is where you can declare all your replicant to import easily into other files,
+ * and to make sure they have any correct settings on startup.
+ */
+export const displaySoundReplicant = nodecg().Replicant<DisplaySound>('displaySound', {
+  defaultValue: {
+    playerId: '',
+  },
+}) as unknown as NodeCG.ServerReplicantWithSchemaDefault<DisplaySound>;
+
+export const runDataActiveRunReplicant = nodecg().Replicant<RunDataActiveRun>('runDataActiveRun', 'nodecg-speedcontrol') as unknown as NodeCG.ServerReplicantWithSchemaDefault<RunDataActiveRun>;
+
+export const informationArrayReplicant = nodecg().Replicant<SetupInformationArray>('setupInformationArray', {
+  defaultValue: [],
+});
+
+export const startSoonReplicant = nodecg().Replicant<StartSoon>('startSoon', {
+  defaultValue: false,
+});
---
+++ src/graphics/example/main.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useHead } from '@unhead/vue';
+import { ref } from 'vue';
+
+const text = ref('Example');
+
+// Set the title of this page.
+useHead({ title: 'example' });
+</script>
+
+<template>
+  <div>
+    {{ text }}
+    <br><br>
+    <img src="./image.png">
+  </div>
+</template>
